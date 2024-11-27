8b30954d2f4658c2e917582ce9b09caf440e45ed
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-11-26 18:53:54 -0800

    prototype

---
+++ index.cjs
@@ -0,0 +1,65 @@
+function parseKeychain(kc) {
+  if (typeof kc === "string") {
+    return kc.split(".");
+  }
+  if (Array.isArray(kc)) {
+    return kc;
+  }
+  throw new Error(
+    `Keychains must either be dot-delimited strings or arrays of keys.\nReceived: ${kc}`
+  );
+}
+
+function get(kc, obj, create = false) {
+  const chain = parseKeychain(kc);
+
+  let pointer = obj;
+  for (const ke of chain) {
+    const nextPointer = pointer[ke];
+    if (nextPointer === undefined || nextPointer === null) {
+      if (create) {
+        pointer[ke] = isNaN(ke) ? {} : [];
+      } else {
+        return null;
+      }
+    }
+    pointer = pointer[ke];
+  }
+  return pointer;
+}
+
+function update(kc, v, obj) {
+  const chain = parseKeychain(kc);
+  const targetKc = chain.slice(0, -1);
+  const target = get(targetKc, obj, true);
+  target[chain[chain.length - 1]] =
+    typeof v === "function" ? v(target[chain[chain.length - 1]]) : v;
+}
+
+function addToSet(kc, v, obj) {
+  update(
+    kc,
+    (prev) => {
+      if (!Array.isArray(prev)) {
+        throw new Error("Attempted to add a list entry to a non-list.");
+      }
+      return [...new Set([...prev, v])];
+    },
+    obj
+  );
+}
+
+function addToList(kc, v, obj) {
+  update(
+    kc,
+    (prev) => {
+      if (!Array.isArray(prev)) {
+        throw new Error("Attempted to add a list entry to a non-list.");
+      }
+      return [...prev, v];
+    },
+    obj
+  );
+}
+
+module.exports = { get, update, addToSet, addToList };
---
+++ mjs.test.js
@@ -0,0 +1,93 @@
+import test from "node:test";
+import { strict as assert } from "node:assert";
+import * as keychain from "./index.mjs";
+
+const getObj = () => ({
+  a: "a",
+  b: {
+    c: "b.c",
+  },
+  c: ["a"],
+});
+
+test("It can find a property.", () => {
+  const obj = getObj();
+  assert.equal(keychain.get("a", obj), obj.a);
+});
+
+test("It can find a nested property.", () => {
+  const obj = getObj();
+  assert.equal(keychain.get("b.c", obj), obj.b.c);
+});
+
+test("It can take an array as a keychain.", () => {
+  const obj = getObj();
+  assert.equal(keychain.get(["b", "c"], obj), obj.b.c);
+});
+
+test("It returns null if not creating and nested property doesn't exist.", () => {
+  const obj = getObj();
+  assert.equal(keychain.get("a.b.c", obj), null);
+});
+
+test("It throws an error on invalid keychain.", () => {
+  assert.throws(() => {
+    keychain.get(1, {});
+  });
+});
+
+test("It can update a property.", () => {
+  const obj = getObj();
+  keychain.update("a", "z", obj);
+  assert.equal(obj.a, "z");
+});
+
+test("It can create a new nested property on update.", () => {
+  const obj = getObj();
+  keychain.update("b.d", "b.d", obj);
+  assert.equal(obj.b.d, "b.d");
+});
+
+test("It can create a new nested property on get.", () => {
+  const obj = getObj();
+  keychain.get("b.d", obj, true);
+  assert.deepEqual(obj.b.d, {});
+});
+
+test("It can add an entry to a set.", () => {
+  const obj = getObj();
+  keychain.addToSet("c", "d", obj);
+  assert.deepEqual(obj.c, ["a", "d"]);
+});
+
+test("It can add an entry to a set, rejecting dupes.", () => {
+  const obj = getObj();
+  keychain.addToSet("c", "a", obj);
+  assert.deepEqual(obj.c, ["a"]);
+});
+
+test("It throws if trying to add to a set that isn't a list.", () => {
+  const obj = getObj();
+  assert.throws(() => {
+    keychain.addToSet("a", "a", obj);
+  });
+});
+
+test("It can add an entry to a list.", () => {
+  const obj = getObj();
+  keychain.addToList("c", "d", obj);
+  assert.deepEqual(obj.c, ["a", "d"]);
+});
+
+test("It can add an entry to a set, allowing dupes.", () => {
+  const obj = getObj();
+  keychain.addToList("c", "a", obj);
+  assert.deepEqual(obj.c, ["a", "a"]);
+});
+
+test("It throws if trying to add to a set that isn't a list.", () => {
+  const obj = getObj();
+  assert.throws(() => {
+    keychain.addToList("a", "a", obj);
+  });
+});
